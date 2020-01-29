e85b8980150fe0143f08594739e81e17a11b8273
zengshide <1909493019@qq.com>
2020-01-29 16:22:57 +0800

    fix: prettier code

--- examples/greet.ts
+++ examples/greet.ts
@@ -1,17 +1,17 @@
-class User{
+class User {
   public fullName: string;
-  constructor(public firstName:string,public lastName:string){
+  constructor(public firstName: string, public lastName: string) {
     this.fullName = firstName + lastName
   }
 }
 interface Person {
-  firstName:string
-  lastName:string
+  firstName: string
+  lastName: string
 }
-function greeter(person:Person) {
+function greeter(person: Person) {
   return `Hello ${person.firstName}-${person.lastName}`;
 }
 
-const user = new User('zeng','shide')
+const user = new User('zeng', 'shide')
 
 console.log(greeter(user));
