c56670b86dd3929e15ea24d2f9d0af392a4859d8
zengshide <1909493019@qq.com>
2020-01-29 16:56:42 +0800

    done: type-array

---
+++ basic-types/type-array.ts
@@ -0,0 +1,8 @@
+// 中括号 类型注解
+let isNumberArray: number[] = []
+isNumberArray = [13]
+console.log(isNumberArray.filter(item => item))
+
+// 泛型 类型注解
+let isStringArray: Array<string> = []
+isStringArray = ['1']
