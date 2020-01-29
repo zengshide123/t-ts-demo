8d1e43d94101b425ff4f0ce29d69cbdccf75dc86
zengshide <1909493019@qq.com>
2020-01-29 16:41:55 +0800

    done: type-number

---
+++ basic-types/type-number.ts
@@ -0,0 +1,12 @@
+// 源 数字类型 不支持包装对象 支持进制
+const decLiteral: number = 20
+const hexLiteral: number = 0x11
+const binaryLiteral: number = 0b11
+const octalLiteral: number = 0o11
+const IsNaN = NaN
+
+// ts 包装过的数字类型 
+const isNumber: Number = new Number(0)
+isNumber.toFixed(1)
+
+
