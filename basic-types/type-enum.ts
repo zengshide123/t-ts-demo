b03ca76cd32cdd630210a42cb469601cd1ef8e4c
zengshide <1909493019@qq.com>
2020-01-29 17:15:15 +0800

    done: type-enum

---
+++ basic-types/type-enum.ts
@@ -0,0 +1,12 @@
+// 枚举 (enum)
+enum Color {
+    Red = 10001,
+    Green = 20001,
+    Blue = 30001
+}
+
+let isBlueValue = Color.Blue
+console.log(isBlueValue)
+let isBlueKey = Color[30001]
+console.log(isBlueKey)
+
