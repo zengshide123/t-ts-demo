586bd5602f1f4a1a80ecb936542af027fa779bcc
zengshide <1909493019@qq.com>
2020-02-01 14:37:35 +0800

    done: type-type

---
+++ type-type/type-type.ts
@@ -0,0 +1,13 @@
+type unionType = {
+    name: string,
+    age: number,
+    shortName?: string
+}
+
+const isUnionType: unionType = {
+    name: 'zeeeeee',
+    age: 18,
+    shortName: '12'
+}
+
+console.log(isUnionType)
