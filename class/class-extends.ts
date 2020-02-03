b451806c50b928aa444942180a7688193f37bb1b
zengshide <1909493019@qq.com>
2020-02-03 14:40:14 +0800

    add: class-extends :construction:

--- class/class-extends.ts
+++ class/class-extends.ts
@@ -1,14 +1,32 @@
 class NewAnimal {
+    name: string
+    constructor(name: string) {
+        this.name = name
+    }
     move(distance: number = 0) {
-        console.log(`Animal moved ${distance}m`)
+        console.log(`${this.name} moved ${distance}m`)
     }
 }
-class Dog extends NewAnimal {
-    bark() {
-        console.log('Woof! Woof!')
+class Snake extends NewAnimal {
+    constructor(name: string) {
+        super(name)
+    }
+    move(dis: number = 5) {
+        console.log('Slithering...')
+        super.move(dis)
     }
 }
+class Horse extends NewAnimal {
+    constructor(name: string) {
+        super(name)
+    }
+    move(dis: number = 45) {
+        console.log('Galloping...')
+        super.move(dis)
+    }
+}
+const snake = new Snake('snake')
+const horse = new Horse('horse')
 
-const dog = new Dog
-dog.bark()
-dog.move(12)
+snake.move()
+horse.move()
