e9ba1a448ef8c804b132ba022eaf972bee5f7a7a
zengshide <1909493019@qq.com>
2020-02-15 15:57:20 +0800

    add:  restt-func-params

--- function/func-params.ts
+++ function/func-params.ts
@@ -1,8 +1,11 @@
-function buildName(firstName: string, lastName = 'Rechial'): string {
-    return firstName + ' ' + lastName
+function buildName(firstName: string, ...restOfName: Array<string>): string {
+    return firstName + ' ' + restOfName.join(' ')
 }
 
 let result1 = buildName('Bob')
-console.log('result1: ', result1);
+console.log('result1: ', result1)
 let result2 = buildName('John', 'Smith')
-console.log('result2: ', result2);
+console.log('result2: ', result2)
+let result3 = buildName('zeng', 'shi', 'de')
+console.log('result3: ', result3);
+
