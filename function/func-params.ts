af790119945afeba2687a7bac770f0fc4bc51ef5
zengshide <1909493019@qq.com>
2020-02-15 15:44:12 +0800

    add: func-params

---
+++ function/func-params.ts
@@ -0,0 +1,6 @@
+function buildName(firstName: string, lastName: string): string {
+    return firstName + ' ' + lastName
+}
+
+let result1 = buildName('Bob','Smith')
+console.log('result1: ', result1);
