a407a2c2e4a53769af17adc2bdefbcd413fb4f15
zengshide <1909493019@qq.com>
2020-02-15 10:36:31 +0800

    add: class-modify-origin

--- class/class-modify.ts
+++ class/class-modify.ts
@@ -1,11 +1,32 @@
 export default class Animal {
-    constructor(private name:string){
+    constructor(private name: string) {
 
     }
-    move(distance:number = 0){
+    move(distance: number = 0) {
         console.log(`${this.name} moved ${distance}m`)
     }
 }
 
-const cat = new Animal('cat')
-cat.move(5)
+class Rhino extends Animal {
+    constructor() {
+        super('Rhino')
+    }
+}
+
+class Employee {
+    constructor(private name: string) {
+
+    }
+    move(distance: number = 5) {
+
+    }
+}
+
+let animal = new Animal('Goat')
+let rhino = new Rhino()
+let employee = new Employee('Blob')
+
+// name 的类型声明用的同一个源
+animal = rhino
+// name 的类型声明用的不是同一个源
+animal = employee
