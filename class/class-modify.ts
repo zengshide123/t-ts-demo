1ccc97d1c9b4222852c121dac98c9aaa7ff25b5b
zengshide <1909493019@qq.com>
2020-02-15 11:03:28 +0800

    add: class-readonly

--- class/class-modify.ts
+++ class/class-modify.ts
@@ -1,26 +1,9 @@
-export default class Person {
-    protected constructor(protected name: string) {
+class Person {
+    constructor(readonly name: string){
 
     }
 }
-class Employee extends Person {
-    constructor(name: string, private department: string) {
-        super(name)
-    }
-    getElevatorPitch(){
-        return `Hello, my name is ${this.name} and I wol in ${this.department}`
-    }
-}
 
-let howard = new Employee('Howard','Sales')
-// private 子类可以使用
-console.log(howard.getElevatorPitch())
-// private 实例不可以使用
-// console.log(howard.name)
-
-
-// constructor
-// public 无限制
-// protected 子类声明,子类不能用
-// private 类声明和实例都不能用
-// let person = new Person('Jhon')
+let  john = new Person('John')
+// name 只能在实例化的时候初始化值
+console.log(john.name)
